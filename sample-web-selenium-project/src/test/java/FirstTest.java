a25cd438ace5facd0f62468f96ac8c18d0a34028
Lena <etarnovskaya@gmail.com>
2018-07-11 21:15:29 +0300

    created class FirstTest

---
+++ sample-web-selenium-project/src/test/java/FirstTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    FirefoxDriver wd;
+    @BeforeMethod
+    public void init(){
+        wd = new FirefoxDriver();
+    }
+
+    @Test
+    public  void firstTestScript(){
+   //     wd.get("https://search.maven.org");
+     wd.navigate().to("https://search.maven.org");
+
+    }
+
+    @AfterMethod
+    public  void stop() throws InterruptedException {
+        Thread.sleep(3000);
+        wd.quit();
+    }
+
+
+
+
+
+
+
+}
